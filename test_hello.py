78c64a2bcb873d0deeccee90ff3ffbf236c1ff31
huy-js <huycntt97@gmail.com>
2022-05-22 16:01:50 +0700

    refactor mail.yml

--- test_hello.py
+++ test_hello.py
@@ -3,6 +3,5 @@ from hello import hello
 
 def test_hello():
     runner = CliRunner()
-    result = runner.invoke(hello, ["--name", "Thor",
-        "--color", "blue"])
+    result = runner.invoke(hello, ["--name", "Thor", "--color", "blue"])
     assert "Thor" in result.output
